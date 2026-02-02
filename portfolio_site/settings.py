e48caa173c262c35d6bfbcb0a5d6e8bbf5e04271
irshadmuhammed <irshadmm16@gmail.com>
2026-02-02 17:58:00 +0530

    Update static files configuration

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -121,8 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+import os
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
